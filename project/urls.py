9dc5ee083df420059690a5208b64cc239150d2a1
bx0-0 <sabermohamedbx@gmail.com>
2024-04-06 16:47:50 +0200

    add some sitting for Deploy

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings 
 from django.conf.urls.static import static 
 from django.views.static import serve
@@ -29,6 +29,9 @@ urlpatterns = [
     path('post/', include('single_blog.urls',namespace='post')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
