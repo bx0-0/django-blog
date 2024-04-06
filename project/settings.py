d997df812a9e426265b1114681922cd57a66c6ea
bx0-0 <sabermohamedbx@gmail.com>
2024-04-06 16:03:37 +0200

    add some sitting for Deploy

--- project/settings.py
+++ project/settings.py
@@ -124,7 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/' 
+if DEBUG:
 
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = BASE_DIR/"assets"
 
 STATICFILES_STORAGE = "django_forgiving_collectstatic.storages.ForgivingManifestStaticFilesStorage"
@@ -140,8 +147,7 @@ STATICFILES_DIRS = [
          BASE_DIR / "static", 
          '/var/www/static/', 
      ] 
-MEDIA_URL = '/media/' 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 ROOT_URLCONF = 'project.urls'
 
 
