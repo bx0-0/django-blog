68401697fbacbd077ae17f788cea3e438b6050fa
bx0-0 <sabermohamedbx@gmail.com>
2024-04-06 15:41:25 +0200

    add some sitting for Deploy

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
     
 #sqlite database
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
